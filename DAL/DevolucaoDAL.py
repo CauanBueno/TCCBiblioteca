6867ce525235d9bcb4f01fe0da6fa305d01074a1
NatiEliz <150460186+NatiEliz@users.noreply.github.com>
2024-03-17 14:56:04 -0300

    Update DevolucaoDAL.py

--- DAL/DevolucaoDAL.py
+++ DAL/DevolucaoDAL.py
@@ -12,7 +12,7 @@ class DevolucaoDAL:
             
             return True
     
-    def listar (self):
+    def listar(self):
         with Conexao() as conexao:
             sql = 'select * from devolucoes'
             conexao.execute(sql)
@@ -20,30 +20,13 @@ class DevolucaoDAL:
             linhas = conexao.fetchall()
             
             if linhas:
-                devolucao: list [Devolucao] = []
+                devolucoes: list[Devolucao] = []
                 
                 for linha in linhas:
-                    fk_idreserva, data_devolucao = linha
-                    devolucao = Devolucao (fk_idreserva, data_devolucao)
+                    id, fk_idreserva, data_devolucao = linha
+                    devolucao = Devolucao (id, fk_idreserva, data_devolucao)
                     
-                return devolucao
-
-
-class RelatorioDAL:
-    def listarRelarorioDAL(self, fk_nome: Reserva, fk_titulo: Reserva, data_reserva: Reserva, data_prevdevol: Reserva, data_devolucao: Devolucao):
-        with Conexao() as conexao:
-            sql = "select nome 'Nome Cliente:', titulo 'Titulo Livro:', data_reserva 'Data da reserva:', data_prevdevol 'Data prevista de devolução:', data_devolucao 'Data real devolução:' from reservas left join devolucoes on devolucoes.fk_idreservas = reservas.id, usuarios on reservas.fk_nome = usuarios.id, livros on reservas.fk_titulo = livros.id"
-            parametros = [fk_nome, fk_titulo, data_reserva, data_prevdevol, data_devolucao]
-            conexao.execute(sql, parametros)
-            linhas = conexao.fetchmany()
-            
-            if linhas:
-                relatorios: list [Reserva, Devolucao] = []
+                    devolucoes.append(devolucao)
                 
-                for linha in linhas:
-                    fk_nome, fk_titulo, data_reserva, data_prevdevol, data_devolucao = linha
-                    relatorio = Reserva (fk_nome, fk_titulo, data_reserva, data_prevdevol), Devolucao (data_devolucao)
-                    
-                    relatorios.append(relatorio)
-                
-                return relatorios
+                return devolucoes
+
